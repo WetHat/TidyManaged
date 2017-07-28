52cf531e35d200649b0bd7f71419737d082a4f2c
WetHat <PeterErnst@t-online.de>
2017-07-28 12:32:11 +0200

    MStest migration

--- TidyManagedTests/DocumentTests.cs
+++ TidyManagedTests/DocumentTests.cs
@@ -1,85 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Palaso.IO;
+using System.IO;
+using System.Text;
 using TidyManaged;
 
 namespace TidyManagedTests
 {
-	[TestFixture]
-	public class DocumentTests
-	{
-		[Test]
-		public void Save_UsingString_RoundTripsUtf8String()
-		{
-			// ŋ --> ?
-			// β (03B2) --> &#65533;
-			using (var tidy = TidyManaged.Document.FromString("<!DOCTYPE html><html><head> <meta charset='UTF-8'></head><body>ŋ β</body></html>"))
-			{
-				tidy.InputCharacterEncoding = EncodingType.Utf8;
-				tidy.OutputCharacterEncoding = EncodingType.Utf8;
+    [TestClass]
+    public class DocumentTests
+    {
+        [TestMethod]
+        public void Save_UsingString_RoundTripsUtf8String()
+        {
+            // ŋ --> ?
+            // β (03B2) --> &#65533;
+            using (var tidy = TidyManaged.Document.FromString("<!DOCTYPE html><html><head> <meta charset='UTF-8'></head><body>ŋ β</body></html>"))
+            {
+                tidy.InputCharacterEncoding = EncodingType.Utf8;
+                tidy.OutputCharacterEncoding = EncodingType.Utf8;
 
-				tidy.CleanAndRepair();
-				using (var temp = new TempFile())
-				{
-					tidy.Save(temp.Path);
-					var newContents = File.ReadAllText(temp.Path);
-					Assert.IsTrue(newContents.Contains("ŋ"), newContents);
-				}
-			}
-		}
-		[Test]
-		public void Save_UsingFile_RoundTripsUtf8File()
-		{
-			// ŋ (velar nasal)--> &#331;
-			// β (greek beta) (03B2) --> &#946;
-			using (var input = TempFile.CreateAndGetPathButDontMakeTheFile())
-			{
-				var source = "<!DOCTYPE html><html><head> <meta charset='UTF-8'></head><body>ŋ β</body></html>";
-				File.WriteAllText(input.Path, source, Encoding.UTF8);
-				using (var tidy = TidyManaged.Document.FromFile(input.Path))
-				{
-					//tidy.CharacterEncoding = EncodingType.Utf8; 
-					tidy.InputCharacterEncoding = EncodingType.Utf8; ;
-					tidy.OutputCharacterEncoding = EncodingType.Utf8;
-					tidy.CleanAndRepair();
-					using (var output = new TempFile())
-					{
-						tidy.Save(output.Path);
-						var newContents = File.ReadAllText(output.Path);
-						Assert.IsTrue(newContents.Contains("ŋ"), newContents);
-					}
-				}
-			}
-		}
-		[Test]
-		public void Save_UsingStream_RoundTripsUtf8File()
-		{
-			// ŋ (velar nasal)--> &#331;
-			// β (greek beta) (03B2) --> &#946;
-			using (var input = TempFile.CreateAndGetPathButDontMakeTheFile())
-			{
-				var source = "<!DOCTYPE html><html><head> <meta charset='UTF-8'></head><body>ŋ β</body></html>";
-				
-				using(var stream = new MemoryStream(Encoding.UTF8.GetBytes(source)))
-				using (var tidy = TidyManaged.Document.FromStream(stream))
-				{
-					tidy.CharacterEncoding = EncodingType.Utf8; 
-//					tidy.InputCharacterEncoding = EncodingType.Utf8; ;
-//					tidy.OutputCharacterEncoding = EncodingType.Utf8;
+                tidy.CleanAndRepair();
+                using (var temp = new TempFile())
+                {
+                    tidy.Save(temp.Path);
+                    var newContents = File.ReadAllText(temp.Path, Encoding.UTF8);
+                    Assert.IsTrue(newContents.Contains("ŋ"), newContents);
+                }
+            }
+        }
 
-					tidy.CleanAndRepair();
-					using (var output = new TempFile())
-					{
-						tidy.Save(output.Path);
-						var newContents = File.ReadAllText(output.Path);
-						Assert.IsTrue(newContents.Contains("ŋ"), newContents);
-					}
-				}
-			}
-		}
-	}
+        [TestMethod]
+        public void Save_UsingFile_RoundTripsUtf8File()
+        {
+            // ŋ (velar nasal)--> &#331;
+            // β (greek beta) (03B2) --> &#946;
+            using (var input = TempFile.CreateAndGetPathButDontMakeTheFile())
+            {
+                var source = "<!DOCTYPE html><html><head> <meta charset='UTF-8'></head><body>ŋ β</body></html>";
+                File.WriteAllText(input.Path, source, Encoding.UTF8);
+                using (var tidy = TidyManaged.Document.FromFile(input.Path))
+                {
+                    //tidy.CharacterEncoding = EncodingType.Utf8;
+                    tidy.InputCharacterEncoding = EncodingType.Utf8; ;
+                    tidy.OutputCharacterEncoding = EncodingType.Utf8;
+                    tidy.CleanAndRepair();
+                    using (var output = new TempFile())
+                    {
+                        tidy.Save(output.Path);
+                        var newContents = File.ReadAllText(output.Path, Encoding.UTF8);
+                        Assert.IsTrue(newContents.Contains("ŋ"), newContents);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Save_UsingStream_RoundTripsUtf8File()
+        {
+            // ŋ (velar nasal)--> &#331;
+            // β (greek beta) (03B2) --> &#946;
+            using (var input = TempFile.CreateAndGetPathButDontMakeTheFile())
+            {
+                var source = "<!DOCTYPE html><html><head> <meta charset='UTF-8'></head><body>ŋ β</body></html>";
+
+                using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(source)))
+                using (var tidy = TidyManaged.Document.FromStream(stream))
+                {
+                    tidy.CharacterEncoding = EncodingType.Utf8;
+                    //					tidy.InputCharacterEncoding = EncodingType.Utf8; ;
+                    //					tidy.OutputCharacterEncoding = EncodingType.Utf8;
+
+                    tidy.CleanAndRepair();
+                    using (var output = new TempFile())
+                    {
+                        tidy.Save(output.Path);
+                        var newContents = File.ReadAllText(output.Path);
+                        Assert.IsTrue(newContents.Contains("ŋ"), newContents);
+                    }
+                }
+            }
+        }
+    }
 }
--- TidyManagedTests/Properties/AssemblyInfo.cs
+++ TidyManagedTests/Properties/AssemblyInfo.cs
@@ -1,36 +1,20 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("TidyManagedTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TidyManagedTests")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4019348f-29a2-4b10-9f79-f500c6935f9e")]
+[assembly: Guid("66a71e4f-2620-430e-879b-1f7374d4e4ed")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
